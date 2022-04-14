4168f5964e40fb2e48c6a69286c561674198efc8
calinafischer <calinafischer@alunos.utfpr.edu.br>
2022-04-14 13:12:20 -0300

    criação do html, css e js

---
+++ codigo.js
@@ -0,0 +1,19 @@
+
+
+$("#botao").click(function (event) {
+    event.preventDefault();
+    let date = $("#data").val()
+    let Vurl = "https://api.nasa.gov/planetary/apod?api_key=Pb1Af2AJ9ZgJZSVSvQJMZlmUQ4UncE7MzAxvuic6&date=" + date
+    
+    $.ajax({
+        url: Vurl,
+        success: function (response){
+            $("#img").attr("src" , response.url)
+        }
+    })
+
+});
+
+
+  
+
