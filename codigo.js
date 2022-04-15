dc1951340cd32c7abc24ac557a8717fed3031260
calinafischer <calinafischer@alunos.utfpr.edu.br>
2022-04-14 21:14:28 -0300

    ajustes finais

--- codigo.js
+++ codigo.js
@@ -5,13 +5,13 @@ $("#botao").click(function (event) {
     let date = $("#data").val()
     let Vurl = "https://api.nasa.gov/planetary/apod?api_key=Pb1Af2AJ9ZgJZSVSvQJMZlmUQ4UncE7MzAxvuic6&date=" + date
     
+    
     $.ajax({
         url: Vurl,
         success: function (response){
             $("#img").attr("src" , response.url)
         }
     })
-
 });
 
 
